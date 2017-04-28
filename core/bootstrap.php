ab5e35c46348d479722b13c84a434d604fbfd948
Ali Fatahi <a.fatahi1992@gmail.com>
2017-04-28 13:16:24 +0430

    make autoload with composer in our project

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -6,11 +6,6 @@ $app = [];
  */
 $app['config'] = require 'config.php';
 
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-
 /**
  * make our db connect as array
  */
